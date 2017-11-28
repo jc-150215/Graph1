6489b1ddf4c4d66c1d44b227dfad5501c0766b02
仁一 伊藤 <150215@jc-21.jp>
2017-11-28 11:59:45 +0900

    プロジェクト ファイルを追加します。

---
+++ Graph1/Graph1/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Graph1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
